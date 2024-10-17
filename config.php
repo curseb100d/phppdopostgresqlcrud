6ebecd83cc3a71cd28ed1b1415f452693c77a478
curseb100d <libyahan123@gmail.com>
2024-10-17 20:31:10 +0800

    Version 1.0.0

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// PostgreSQL server configuration
+$serverName = "localhost";
+$dbUsername = "postgres";
+$dbPassword = "root";
+$dbName = "postgre_db"
+
+// Create database connection
+try {
+    $conn = new PDO("pgsql: host = ")
+}
+?>
